3fc75afd3900418317245dd6d87ead724327b167
yuns-emre <yunstnrv@gmail.com>
2024-06-29 13:32:10 +0300

    edited firebase service

--- lib/core/services/firebase_service.dart
+++ lib/core/services/firebase_service.dart
@@ -1,11 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseService {
-  init() {
-    signInAnonimously();
-  }
-
-  signInAnonimously() async {
-    await FirebaseAuth.instance.signInAnonymously();
-  }
+  
 }

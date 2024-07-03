7f79ae76d1ae81a18843c357673460511347b655
yuns-emre <yunstnrv@gmail.com>
2024-07-03 15:20:45 +0300

    edited bottom nav item

--- lib/common/helpers/create_bottom_nav_item.dart
+++ lib/common/helpers/create_bottom_nav_item.dart
@@ -2,13 +2,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:oua_flutter33/ui/home/home_view.dart';
+import 'package:oua_flutter33/ui/profile/profile_view.dart';
+import 'package:oua_flutter33/ui/search/search_view.dart';
 
 BottomNavigationBarItem createNavItem(TabItem tabItem) {
   final currentTab = TabItemData.tabs[tabItem]!;
   return BottomNavigationBarItem(icon: currentTab.icon, label: currentTab.title);
 }
 
-enum TabItem { Home, Search, Orders, Favorite }
+enum TabItem { Home, Search, Orders, Profile }
 
 class TabItemData {
   String title;
@@ -21,7 +23,7 @@ class TabItemData {
     TabItem.Home: TabItemData(icon: const Icon(Icons.home), title: "Home"),
     TabItem.Search: TabItemData(icon: const Icon(Icons.search), title: "Search"),
     TabItem.Orders: TabItemData(icon: const Icon(Icons.add_box), title: "Orders"),
-    TabItem.Favorite: TabItemData(icon: const Icon(Icons.favorite), title: "Profile"),
+    TabItem.Profile: TabItemData(icon: const Icon(Icons.person), title: "Profile"),
   };
 }
 
@@ -30,11 +32,11 @@ Widget getViewForIndex(int index) {
     case 0:
       return const HomeView();
     case 1:
-      return HomeView();
+      return const SearchView();
     case 2:
       return HomeView();
     case 3:
-      return HomeView();
+      return const ProfileView();
     default:
       return const HomeView();
   }

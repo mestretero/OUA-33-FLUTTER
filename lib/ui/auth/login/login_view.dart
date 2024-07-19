981b9ba2ad2a9bf6a57f32a0747d3b727eb556ff
yuns-emre <yunstnrv@gmail.com>
2024-07-19 16:23:19 +0300

    edited login and register

--- lib/ui/auth/login/login_view.dart
+++ lib/ui/auth/login/login_view.dart
@@ -2,9 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:oua_flutter33/app/app.router.dart';
-import 'package:oua_flutter33/common/widgets/app_button.dart';
-import 'package:oua_flutter33/common/widgets/custom_textfield.dart';
-import 'package:oua_flutter33/common/widgets/top_title.dart';
+import 'package:oua_flutter33/common/widgets/my_button.dart';
+import 'package:oua_flutter33/common/widgets/my_texfield.dart';
 import 'package:oua_flutter33/ui/auth/login/login_view_model.dart';
 import 'package:stacked/stacked.dart';
 
@@ -27,26 +26,53 @@ class LoginView extends StatelessWidget {
             child: Form(
               key: formKey,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const TopTitle(
-                    title: 'Login',
-                    subTitle: 'Welcome back',
+                  Image.asset(
+                    "assets/images/master-hands-logo.png",
+                    height: 200,
+                    width: 200,
                   ),
-                  CustomTextField(
+                  Text(
+                  "Giriş Yap",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  "Hayatın her anında el yapımı bir dokunuş arayanlar için Usta Eller özenle seçilmiş el işleri ve kişisel dokunuşlarla dolu bir dünyaya kapı aralıyor.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                  MyTextField(
                     controller: emailController,
-                    name: 'Email',
-                    prefixIcon: Icons.mail_outline,
+                    name: "Email Adresiniz",
+                    hintText: "Email Adresiniz",
                     inputType: TextInputType.emailAddress,
+                    prefixIcon: Icons.mail_outline,
+                    isTextArea: false,
                   ),
-                  CustomTextField(
+                  MyTextField(
                     controller: passController,
-                    name: 'Password',
-                    prefixIcon: Icons.lock_outline,
+                    name: "Password",
+                    hintText: "Password",
                     inputType: TextInputType.text,
+                    prefixIcon: Icons.lock_outline,
+                    isTextArea: false,
                     obscureText: true,
                   ),
-                  AppButton(
+                  const SizedBox(height: 16),
+                  MyButton(
                     onTap: () {
                       if (formKey.currentState!.validate()) {
                         model.authServices.login(
@@ -58,21 +84,38 @@ class LoginView extends StatelessWidget {
                         model.navigationService.navigateTo(Routes.mainView);
                       }
                     },
-                    text: "Login",
+                    text: "Giriş Yap",
+                    buttonStyle: 1,
+                    isExpanded: true,
                   ),
-                  const SizedBox(height: 30),
-                  Center(
-                    child: TextButton(
-                      onPressed: () {
-                        model.navigationService
-                            .navigateTo(Routes.registerView)
-                            ?.then((_) {
-                          formKey.currentState?.reset();
-                          FocusScope.of(context).requestFocus(FocusNode());
-                        });
-                      },
-                      child: const Text("Register"),
-                    ),
+                  const SizedBox(height: 16),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Hesabın mevcut değil mi?",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          model.navigationService
+                              .navigateTo(Routes.registerView)
+                              ?.then((_) {
+                            formKey.currentState?.reset();
+                            FocusScope.of(context).requestFocus(FocusNode());
+                          });
+                        },
+                        child: Text(
+                          "Kayıt Ol",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
--- lib/ui/auth/register/register_view.dart
+++ lib/ui/auth/register/register_view.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
-import 'package:oua_flutter33/common/widgets/app_button.dart';
-import 'package:oua_flutter33/common/widgets/custom_textfield.dart';
-import 'package:oua_flutter33/common/widgets/top_title.dart';
+import 'package:oua_flutter33/app/app.router.dart';
+import 'package:oua_flutter33/common/widgets/my_button.dart';
+import 'package:oua_flutter33/common/widgets/my_texfield.dart';
 import 'package:oua_flutter33/ui/auth/register/register_view_model.dart';
 import 'package:stacked/stacked.dart';
 
@@ -28,40 +28,70 @@ class RegisterView extends StatelessWidget {
             child: Form(
               key: formKey,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const TopTitle(
-                    title: 'Register',
-                    subTitle: 'Create an account',
+                  Image.asset(
+                    "assets/images/master-hands-logo.png",
+                    height: 200,
+                    width: 200,
                   ),
-                  CustomTextField(
+                  Text(
+                    "Kayıt Ol",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 32,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Hayatın her anında el yapımı bir dokunuş arayanlar için Usta Eller özenle seçilmiş el işleri ve kişisel dokunuşlarla dolu bir dünyaya kapı aralıyor.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  MyTextField(
                     controller: nameController,
-                    name: 'Name',
-                    prefixIcon: Icons.person_outline,
+                    name: "Name",
+                    hintText: "Name",
                     inputType: TextInputType.name,
+                    prefixIcon: Icons.person_outline,
+                    isTextArea: false,
                     textCapitalization: TextCapitalization.words,
                   ),
-                  CustomTextField(
+                  MyTextField(
                     controller: surnameController,
-                    name: 'Surname',
-                    prefixIcon: Icons.person_outline,
+                    name: "Surname",
+                    hintText: "Surname",
                     inputType: TextInputType.name,
+                    prefixIcon: Icons.person_outline,
+                    isTextArea: false,
                     textCapitalization: TextCapitalization.words,
                   ),
-                  CustomTextField(
+                  MyTextField(
                     controller: emailController,
-                    name: 'Email',
-                    prefixIcon: Icons.mail_outline,
+                    name: "Email",
+                    hintText: "Email",
                     inputType: TextInputType.emailAddress,
+                    prefixIcon: Icons.mail_outline,
+                    isTextArea: false,
                   ),
-                  CustomTextField(
+                  MyTextField(
                     controller: passController,
-                    name: 'Password',
-                    prefixIcon: Icons.lock_outline,
+                    name: "Password",
+                    hintText: "Password",
                     inputType: TextInputType.text,
+                    prefixIcon: Icons.lock_outline,
+                    isTextArea: false,
                     obscureText: true,
                   ),
-                  AppButton(
+                  const SizedBox(height: 16),
+                  MyButton(
                     onTap: () {
                       if (formKey.currentState!.validate()) {
                         model.authServices.register(
@@ -73,14 +103,38 @@ class RegisterView extends StatelessWidget {
                         );
                       }
                     },
-                    text: "Register",
+                    text: "Kayıt Ol",
+                    buttonStyle: 1,
+                    isExpanded: true,
                   ),
-                  const SizedBox(height: 30),
-                  Center(
-                    child: TextButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text("Login"),
-                    ),
+                  const SizedBox(height: 16),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Zaten hesabınız var mı?",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          model.navigationService
+                              .navigateTo(Routes.loginView)
+                              ?.then((_) {
+                            formKey.currentState?.reset();
+                            FocusScope.of(context).requestFocus(FocusNode());
+                          });
+                        },
+                        child: Text(
+                          "Giriş Yap",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onPrimary,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
